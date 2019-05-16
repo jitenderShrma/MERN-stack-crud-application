16b5781e556281276a9cfa02b6b1e9c44d7840e1
jitenderKumar97 <jitenderkanti020@gmail.com>
2019-05-17 00:19:56 +0530

    change in package.json

--- app.js
+++ app.js
@@ -23,16 +23,11 @@ app.use('/merchant', index);
 
 // Production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname = 'client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-// Build mode
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
 
 // Listen to server
 const port = process.env.PORT || 5000;
